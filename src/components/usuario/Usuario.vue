0644edfd31099132320733a39f868fc4904f1368
Gaspar De Carlo <gaspar.decarlo@gmail.com>
2021-06-01 09:55:17 -0300

    se agrega funcionalidad para consumo de servicios

--- src/components/usuario/Usuario.vue
+++ src/components/usuario/Usuario.vue
@@ -1,15 +1,54 @@
- <template>
-   <div>
-       <h1>Usuarios</h1>
-   </div>
- </template>
- 
- <script>
- export default {
- 
- }
- </script>
- 
- <style>
- 
- </style>
+<template>
+  <div>
+    <h1>Usuarios</h1>
+    <button @click="inicializar">Traer usuarios</button>
+    <button @click="saveUsuario">Save usuario</button>
+    <button @click="deleteUsuario">Delete usuario</button>
+    <!-- <samp>{{ usuarios }}</samp> -->
+    <ul>
+      <li v-for="usuario in usuarios" :key="usuario.id">
+        {{ usuario.id +'-'+ usuario.username }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    
+    return {
+      usuarios: [],
+      url: "https://60b40b584ecdc10017480255.mockapi.io/api/usuarios",
+    };
+  },
+  methods: {
+    async inicializar(){
+      await this.getUsuarios()
+    },
+    async getUsuarios() {
+      try {
+        let response = await axios.get(this.url);
+        this.usuarios = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async saveUsuario(){
+      const usuario = {
+        username:'Jose'
+      }
+      let respuesta = await axios.post(this.url,usuario)
+      console.log(respuesta)
+    },
+    async deleteUsuario(){
+      let respuesta = await axios.delete(this.url+'/1')
+      console.log(respuesta)
+    }
+  },
+};
+</script>
+
+<style></style>
